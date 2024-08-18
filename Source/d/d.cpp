c7d836bc186a3a8a5913dd1a0d65f49e46890cd8
YSChoi <ghs00456@gmail.com>
2024-08-19 01:51:06 +0900

    Update

--- Source/d/d.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "d.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, d, "d" );
-
-DEFINE_LOG_CATEGORY(Logd)
- 
---
+++ practice/Source/practice/practice.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "practice.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, practice, "practice" );
+
+DEFINE_LOG_CATEGORY(Logpractice)
+ 
